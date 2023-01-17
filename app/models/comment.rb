5ffcf9513ec3aba0bb590d44ee8c0f2fcbe1b322
Emre Karayaman <karayaman.emre@outlook.com>
2023-01-17 17:37:48 +0300

    required changes made

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
-  belongs_to :user
+  belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  after_save :update_comments_counter
 
   def update_comments_counter
     post.update(comments_counter: post.comments_counter + 1)
