a04543580f7a5c34aaed427afba37c125ed68295
Emre Karayaman <karayaman.emre@outlook.com>
2023-01-17 16:25:35 +0300

    custom methods created

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "author_id"
   has_many :comments
   has_many :likes
+
+  def update_posts_counter
+    author.update(posts_counter: author.posts_counter + 1)
+  end
+
+  def recent_5_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
