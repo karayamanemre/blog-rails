e8fedd139e0808f078416210811a2e9d17122094
Emre Karayaman <karayaman.emre@outlook.com>
2023-01-24 12:38:52 +0300

    linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
-  def index;
+  def index
     @user = User.find(params[:id])
     @user_posts = @user.posts
     @user_posts = @user.posts.includes(:comments)
     @comments = Comment.all
   end
 
-  def show; 
+  def show
     @post = Post.find(params[:id])
     @comments = @post.comments
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
-  def index;
+  def index
     @users = User.all
   end
-  def show;
+  def show
     @user = User.find(params[:id])
   end
 end
