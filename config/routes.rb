2082edfc59442c0a7d16f32565ae4af6726d4ed9
Emre Karayaman <karayaman.emre@outlook.com>
2023-01-24 18:59:18 +0300

    routes created and form added

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,9 @@ Rails.application.routes.draw do
   get "/users/:id", to: "users#show"
   get "users/:id/posts", to: "posts#index"
   get "users/:id/posts/:id", to: "posts#show"
+  get "/posts/new", to: "posts#new"
+  post "posts/new", to: "posts#create"
+  get '/comments/new', to: "comments#new"
+  post '/comments/new', to: "comments#create"
+  post '/likes/new', to: "likes#create"
 end
