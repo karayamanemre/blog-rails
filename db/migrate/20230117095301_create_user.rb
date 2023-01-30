29a95c8ab9b761432d725c1b1261c7e90d659ee4
Emre Karayaman <karayaman.emre@outlook.com>
2023-01-30 18:16:15 +0300

    column names fixed

--- db/migrate/20230117095301_create_user.rb
+++ db/migrate/20230117095301_create_user.rb
@@ -3,9 +3,9 @@
 class CreateUser < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :Name
-      t.string :Photo
-      t.text :Bio
+      t.string :name
+      t.string :photo
+      t.text :bio
       t.integer :posts_counter
 
       t.timestamps
--- db/migrate/20230117095742_create_comment.rb
+++ db/migrate/20230117095742_create_comment.rb
@@ -3,7 +3,7 @@
 class CreateComment < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.text :Text
+      t.text :text
 
       t.timestamps
     end
