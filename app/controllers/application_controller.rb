7efe937623cec4479c60be8de9a65f774fe4da87
Emre Karayaman <karayaman.emre@outlook.com>
2023-01-30 19:35:21 +0300

    updated views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :update_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def update_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :photo, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name email bio photo password current_password])
+  end
 end
