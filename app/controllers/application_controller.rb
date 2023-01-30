618f47bc017e60d69dbdbdc998bf817850d33cdc
Emre Karayaman <karayaman.emre@outlook.com>
2023-01-31 00:31:33 +0300

    updated views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   protect_from_forgery with: :exception
   before_action :update_permitted_parameters, if: :devise_controller?
 
@@ -6,6 +7,6 @@ class ApplicationController < ActionController::Base
 
   def update_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :photo, :email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name email bio photo password current_password])
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :bio, :photo, :email, :password, :current_password) }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,18 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "users#index"
-  get "/users/:id", to: "users#show"
-  get "users/:id/posts", to: "posts#index"
-  get "users/:id/posts/:id", to: "posts#show"
-  get "/posts/new", to: "posts#new"
-  post "posts/new", to: "posts#create"
-  get '/comments/new', to: "comments#new"
-  post '/comments/new', to: "comments#create"
-  post '/likes/new', to: "likes#create"
+  # Defines the root path route ('/')
+  root 'users#index'
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
+  get 'users/:id/posts', to: 'posts#index'
+  get 'users/:id/posts/:id', to: 'posts#show'
+  get '/posts/new', to: 'posts#new'
+  post 'posts/new', to: 'posts#create'
+  get '/comments/new', to: 'comments#new'
+  post '/comments/new', to: 'comments#create'
+  post '/likes/new', to: 'likes#create'
   resources :posts
+  resources :users
 end
