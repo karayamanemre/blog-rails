48f1d4e64f8ad9b02dac2f0e5da86c6d971edaef
Emre Karayaman <karayaman.emre@outlook.com>
2023-02-01 17:51:47 +0300

    linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   load_and_authorize_resource
-  
+
   def index
     @user = User.includes(:posts, :comments).find(params[:user_id])
     @user_posts = @user.posts.includes(:author, :comments, :likes).all
